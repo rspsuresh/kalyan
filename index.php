f0b273cd1dcb0fd9142d92c040ccac5ed879ce0c
suresh rampaul <33367078+rspsuresh@users.noreply.github.com>
2019-05-29 13:25:36 +0530

    fullscreen
    fullscren code

--- index.php
+++ index.php
@@ -207,3 +207,57 @@ $("#testform").submit(function(e) {
     }		 
 });	
 </script>
+
+
+
+<h2 (click)="openFullscreen()">open</h2>
+<h2 (click)="closeFullscreen()">close</h2>
+
+
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private document: any) {}
+  elem;
+
+  ngOnInit() {
+    this.elem = document.documentElement;
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
+  }
+}
